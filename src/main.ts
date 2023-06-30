c104837f513ec3538fa56b1f1c0998efb960f095
abernatskiy <abernatskiy@toha.li>
2023-07-01 05:26:12 +0900

    Simplifying the codebase
    	modified:   schema.graphql
    	modified:   src/main.ts
    	deleted:    src/processor.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,21 @@
+import {EvmBatchProcessor} from '@subsquid/evm-processor'
 import {TypeormDatabase} from '@subsquid/typeorm-store'
 import {CreatedContract} from './model'
-import {processor} from './processor'
+import {lookupArchive} from '@subsquid/archive-registry'
+
+const processor = new EvmBatchProcessor()
+    .setDataSource({
+        archive: lookupArchive('eth-mainnet'),
+    })
+    .setFields({
+        trace: {
+            createResultAddress: true,
+        },
+    })
+    .addTrace({
+        type: ['create'],
+        transaction: true,
+    })
 
 processor.run(new TypeormDatabase({supportHotBlocks: false}), async (ctx) => {
     const contracts: Map<string, CreatedContract> = new Map()
@@ -8,21 +23,9 @@ processor.run(new TypeormDatabase({supportHotBlocks: false}), async (ctx) => {
     for (let c of ctx.blocks) {
         for (let trc of c.traces) {
             if (trc.type === 'create' && trc.result?.address != null && trc.transaction?.hash !== undefined) {
-                contracts.set(trc.result.address, new CreatedContract({id: trc.result.address})
-/*
-                    new CreatedContract({
-                        id: `${trc.transaction.hash}-${trc.result.address}-${trc.transactionIndex}-${c.header.height}`,
-                        block: c.header.height,
-                        timestamp: new Date(c.header.timestamp),
-                        address: trc.result.address,
-//                        code: trc.result.code,
-                        txHash: trc.transaction.hash,
-                    })
-*/
-                )
+                contracts.set(trc.result.address, new CreatedContract({id: trc.result.address}))
             }
         }
     }
-//    await ctx.store.insert(contracts)
     await ctx.store.upsert([...contracts.values()])
 })
